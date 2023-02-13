a06201dd4cfff5783094eb33b471948be959b3dc
stefany <stefany.silveira21@gmail.com>
2023-02-13 14:13:12 -0300

    Java OO: entendendo a Orientação a Objetos

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -7,25 +7,57 @@ public class Conta {
     int numero;
     String titular;
 
-    public void deposita(double valor) {
-        this.saldo += valor;
+    public Conta() {
+
     }
 
-    public boolean saca(double valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
-            return true;
-        } else {
-            return false;
-        }
+
+    public Conta(double saldo, int agencia, int numero, String titular) {
+        this.saldo = saldo;
+        this.agencia = agencia;
+        this.numero = numero;
+        this.titular = titular;
     }
 
-    public boolean transfere(double valor, Conta destino) {
-        if (saldo >= valor) {
-            saldo -= valor;
-            destino.deposita(valor);
-            return true;
-        }
-        return false;
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "saldo=" + saldo +
+                ", agencia=" + agencia +
+                ", numero=" + numero +
+                ", titular='" + titular + '\'' +
+                '}';
     }
 }
