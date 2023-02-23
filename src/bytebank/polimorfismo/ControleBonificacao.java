03b84493947d9d33fccc49fb76cb8a69de7152fa
stefany <stefany.silveira21@gmail.com>
2023-02-22 21:26:12 -0300

    Java Polimorfismo: entenda herança e interfaces - [Entendendo o Polimorfismo]

---
+++ src/bytebank/polimorfismo/ControleBonificacao.java
@@ -0,0 +1,14 @@
+package bytebank.polimorfismo;
+
+public class ControleBonificacao {
+    private double soma;
+
+    public void registra(Funcionario f) {
+        double boni = f.getBonificacao();
+        this.soma = this.soma + boni;
+    }
+
+    public double getSoma() {
+        return this.soma;
+    }
+}
---
+++ src/bytebank/polimorfismo/Gerente.java
@@ -0,0 +1,22 @@
+package bytebank.polimorfismo;
+
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public double getBonificacao() {
+        System.out.println("Chamando o método de bonificação do Gerente");
+        return super.getBonificacao() + super.getSalario();
+    }
+}
