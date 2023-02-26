898a854f9771deb14d25c0bc0b35c18d419ca2ea
Stefany Silveira <stefany.silveira21@gmail.com>
2023-02-26 14:38:11 -0300

    Java Polimorfismo: entenda herança e interfaces - [Classe abstrata]

--- src/bytebank/polimorfismo/TesteFuncionario.java
+++ src/bytebank/polimorfismo/TesteFuncionario.java
@@ -1,11 +1,12 @@
 package bytebank.polimorfismo;
 
 import bytebank.polimorfismo.herdado.Funcionario;
+import bytebank.polimorfismo.herdado.Gerente;
 
 public class TesteFuncionario {
     public static void main(String[] args) {
 
-        Funcionario nico = new Funcionario();
+        Funcionario nico = new Gerente();
         nico.setNome("Nico Steppat");
         nico.setCpf("111.222.333.44");
         nico.setSalario(2600.00);
