d018b780cdf3d45d680d736af8bda580eeb75bcf
stefany <stefany.silveira21@gmail.com>
2023-02-28 14:03:46 -0300

    Java Polimorfismo: entenda herança e interfaces - [Mais uma classe abstrata]

---
+++ src/bytebank/polimorfismo/Administrador.java
@@ -0,0 +1,15 @@
+package bytebank.polimorfismo;
+
+public class Administrador extends FuncionarioAutenticavel {
+    private int senha;
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public double getBonificacao() {
+        return 50;
+    }
+
+}
--- src/bytebank/polimorfismo/Gerente.java
+++ src/bytebank/polimorfismo/Gerente.java
@@ -1,15 +1,7 @@
 package bytebank.polimorfismo;
 
-public class Gerente extends Funcionario {
+public class Gerente extends FuncionarioAutenticavel {
     private int senha;
-
-    public boolean autentica(int senha) {
-        if (this.senha == senha) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     public void setSenha(int senha) {
         this.senha = senha;
