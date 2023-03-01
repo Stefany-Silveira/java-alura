92daaecc987caefa18ee2b903301010b8fb795af
stefany <stefany.silveira21@gmail.com>
2023-03-01 16:28:17 -0300

    Java Polimorfismo: entenda herança e interfaces - [Continuando o projeto]

--- src/bytebank/polimorfismo/Administrador.java
+++ src/bytebank/polimorfismo/Administrador.java
@@ -1,20 +1,20 @@
 package bytebank.polimorfismo;
 
 public class Administrador extends Funcionario implements Autenticavel {
-    private int senha;
+    private AutenticacaoUtil autenticador;
+
+    public Administrador() {
+        this.autenticador = new AutenticacaoUtil();
+    }
 
     public void setSenha(int senha) {
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autentica(int senha) {
-        if (this.senha == senha) {
-            return true;
-        } else {
-            return false;
+            return this.autenticador.autentica(senha);
         }
-    }
 
     @Override
     public double getBonificacao() {
