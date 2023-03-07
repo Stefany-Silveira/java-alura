78f4a2ea951283d53a411438e3ec7cf3b4d5be28
stefany <stefany.silveira21@gmail.com>
2023-03-07 14:14:07 -0300

    Java Exceções: aprensa a ccriari, lançar e controlar exceções - [Checked e Unchecked]

--- src/pilha/Fluxo.java
+++ src/pilha/Fluxo.java
@@ -5,7 +5,7 @@ public class Fluxo {
         System.out.println("Ini do main");
         try {
             metodo1();
-        } catch (ArithmeticException | NullPointerException e) {
+        } catch (ArithmeticException | NullPointerException | MinhaExcecao e) {
             String msg = e.getMessage();
             System.out.println("ArithmeticException " + msg);
             e.printStackTrace();
@@ -13,19 +13,20 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws MinhaExcecao {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do método1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws MinhaExcecao {
         System.out.println("Ini do metodo2");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            Conta conta = null;
-            conta.deposita();
-        }
-        System.out.println("Fim do metodo2");
+        throw new MinhaExcecao("deu muito errado");
+        //for (int i = 1; i <= 5; i++) {
+        //   System.out.println(i);
+        // Conta conta = null;
+        // conta.deposita();
+        // }
+        // System.out.println("Fim do metodo2");
     }
 }
