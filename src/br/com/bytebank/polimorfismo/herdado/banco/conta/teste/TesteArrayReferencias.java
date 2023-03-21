e36713a8d61738f2f887b2141ec876f35151445d
stefany <stefany.silveira21@gmail.com>
2023-03-21 18:27:39 -0300

    Java e java.util: Coleções, Wrappers e Lambda expressions - [arrays]

--- src/br/com/bytebank/polimorfismo/herdado/banco/conta/teste/TesteArrayReferencias.java
+++ src/br/com/bytebank/polimorfismo/herdado/banco/conta/teste/TesteArrayReferencias.java
@@ -2,21 +2,26 @@ package br.com.bytebank.polimorfismo.herdado.banco.conta.teste;
 
 import br.com.bytebank.polimorfismo.herdado.banco.conta.modelo.Conta;
 import br.com.bytebank.polimorfismo.herdado.banco.conta.modelo.ContaCorrente;
+import br.com.bytebank.polimorfismo.herdado.banco.conta.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
     public static void main(String[] args) {
 
-        ContaCorrente[] contas = new ContaCorrente[5];
+        //alterando o tipo
+        Conta[] contas = new Conta[5];
 
         ContaCorrente cc1 = new ContaCorrente(22, 11);
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
-
         contas[0] = cc1;
+
+        //cria instancia de ContaPoupanca
+        ContaPoupanca cc2 = new ContaPoupanca(22, 22);
         contas[1] = cc2;
 
-        System.out.println(contas[0].getNumero()
+        System.out.println(contas[1].getNumero());
 
-
-        );
+        // alterou o tipo, realizando o cast
+        ContaCorrente ref = (ContaCorrente) contas[0];
+        System.out.println(cc2.getNumero());
+        System.out.println(ref.getNumero());
     }
 }
