e67bd3d344e3c075fd0bd92807aeb2122dc2d52a
Stefany Silveira <stefany.silveira21@gmail.com>
2023-04-22 12:53:37 -0300

    Java e java.util: Coleções, Wrappers e Lambda expressions - [Vector e Collection]

--- src/br/com/bytebank/polimorfismo/herdado/banco/conta/teste/util/TesteArrayListEquals.java
+++ src/br/com/bytebank/polimorfismo/herdado/banco/conta/teste/util/TesteArrayListEquals.java
@@ -4,14 +4,19 @@ import br.com.bytebank.polimorfismo.herdado.banco.conta.modelo.Conta;
 import br.com.bytebank.polimorfismo.herdado.banco.conta.modelo.ContaCorrente;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 
 
 public class TesteArrayListEquals {
     public static void main(String[] args) {
 
+//        List<Conta> lista = new ArrayList<Conta>(); // usa array por baixo
+//        List<Conta> lista = new LinkedList<Conta>(); // lista linkada
+//        List<Conta> lista = new Vector<Conta>(); // usa array por baixo, mas tb é threadsafe
+
         ArrayList<Conta> lista = new ArrayList<Conta>();
-
-
 
         Conta cc1 = new ContaCorrente(22, 11);
         Conta cc2 = new ContaCorrente(22, 22);
