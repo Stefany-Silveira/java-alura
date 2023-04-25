6012bcfba450b707610a103bc3a03cdbb0c52375
stefany <stefany.silveira21@gmail.com>
2023-04-25 19:26:34 -0300

    Java: trabalhando com listas e coleções de dados [Identificando o objeto]

--- src/br/com/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/screenmatch/principal/PrincipalComListas.java
@@ -10,8 +10,14 @@ public class PrincipalComListas {
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme("O Poderoso Chefão", 1970);
+        meuFilme.avalia(9);
+
         Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+
         Filme filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+
         Serie lost = new Serie("Lost", 2000);
 
         ArrayList<Titulo> lista = new ArrayList<>();
@@ -21,9 +27,10 @@ public class PrincipalComListas {
         lista.add(lost);
 
         for (Titulo item : lista) {
-            System.out.println(item.getNome());
-            Filme filme = (Filme) item;
-            System.out.println("Classificação: " + filme.getClassificacao());
+            System.out.println("Nome: " + item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificacao: " + filme.getClassificacao());
+            }
         }
     }
 }
